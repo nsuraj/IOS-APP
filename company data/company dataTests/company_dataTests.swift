0dd8deaa7ec6b360076e28532aa5c803415e8dc4
suraj <nsurajrajan@gmail.com>
2020-11-13 10:55:56 +0530

    Add files via upload
    IOS App designed to view the companies for placement

---
+++ company data/company dataTests/company_dataTests.swift
@@ -0,0 +1,36 @@
+//
+//  company_dataTests.swift
+//  company dataTests
+//
+//  Created by exam1001 on 06/10/16.
+//  Copyright © 2016 exam1001. All rights reserved.
+//
+
+import XCTest
+@testable import company_data
+
+class company_dataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
